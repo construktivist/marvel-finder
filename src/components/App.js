949e56f639c2092fa7150e0e0c3f602df6292419
Keegan Kelly <keegankelly1@gmail.com>
2020-04-28 14:08:37 -0500

    Setting up router.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 // import logo from '../logo.svg';
 import '../App.css';
 
 class App extends React.Component {
   render () {
     return (
-      <h1>Marvel Finder</h1>
+      <Router>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/foo">Foo</a></li>
+          <li><a href="/bar">Bar</a></li>
+        </ul>
+        <Route path="/" render={() => <Home />} />
+        <Route path="/foo" render={() => <About />} />
+      </Router>
+    )
+  }
+}
+
+class Home extends React.Component {
+  render () {
+    return (
+        <h1>Welcome!</h1>
+    )
+  }
+}
+
+class About extends React.Component {
+  render () {
+    return (
+        <h1>About</h1>
     )
   }
 }
