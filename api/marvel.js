b62459222446ca9947d083f8867b75627b607a85
Keegan Kelly <keegankelly1@gmail.com>
2020-05-06 13:43:38 -0500

    Moved api call to helper file. Verified the call still works.

---
+++ api/marvel.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const md5 = require('md5');
+require('dotenv').config()
+
+const ts = Date.now();
+const hash = md5(ts + process.env.PRIVATE_API_KEY + process.env.PUBLIC_API_KEY);
+
+exports.marvel = {
+
+    find: () => {
+        axios.get('https://gateway.marvel.com:443/v1/public/characters?name=punisher&ts=' + ts + '&apikey='+ process.env.PUBLIC_API_KEY + '&hash=' + hash)
+            .then(function (response) {
+                console.log(response)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+}
+
+
+
--- server.js
+++ server.js
@@ -1,26 +1,12 @@
 const express = require(`express`);
-const axios = require('axios');
-const md5 = require('md5');
-
-
-require('dotenv').config()
+const api = require('./api/marvel.js')
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const ts = Date.now();
-const hash = md5(ts + process.env.PRIVATE_API_KEY + process.env.PUBLIC_API_KEY);
-console.log(hash);
 
-axios.get('https://gateway.marvel.com:443/v1/public/characters?name=punisher&ts=' + ts + '&apikey='+ process.env.PUBLIC_API_KEY + '&hash=' + hash)
-    .then(function (response) {
-        console.log(response)
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
-
+console.log(api.marvel.find())
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ` + PORT)
