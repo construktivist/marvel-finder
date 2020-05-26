357cf9bca6a0b7fd88e74a803cb964018777b5ea
Keegan Kelly <keegankelly1@gmail.com>
2020-05-26 13:15:44 -0500

    Added functionality for react to handle search input.

--- client/src/Components/Search.js
+++ client/src/Components/Search.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
 class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        search: ''
+        }
+    }
+
+    inputChange = (event) => {
+        this.setState({
+            [event.target.search]: event.target.value
+        })
+    }
+
     render () {
         return (
             <form>
-                <input class="search" type="text" value="" autoComplete="off" aria-autoComplete="list" aria-controls="react-listbox" placeholder="Search" />
-                <div class="listbox" id="react-listbox" ></div>
+                <input 
+                    id="search" 
+                    name="search"
+                    className="search" 
+                    type="text" 
+
+                    value={this.state.search}
+                    onChange={this.inputChange} />
+                <div className="listbox" id="react-listbox" ></div>
             </form>
         )
     }
