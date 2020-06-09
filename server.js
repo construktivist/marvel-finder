bbc039978dbdfb10b0b3c38f9f802f930318d4e0
Keegan Kelly <keegankelly1@gmail.com>
2020-06-09 07:32:25 -0500

    Navigating api json

--- server.js
+++ server.js
@@ -22,12 +22,22 @@ app.get('/character', () => {
 
 axios.get('https://gateway.marvel.com:443/v1/public/characters', {
     params: {
-        name: 'deadpool',
+        nameStartsWith: 'deadpool',
         ts: ts,
         apikey: process.env.PUBLIC_API_KEY,
         hash: hash
-
     }
+})            
+.then(function (response) {
+    const results = response.data.data.results;
+    for (let i = 0; i < results.length; i++) {
+        console.log(results[i].name);
+        console.log(results[i].id);
+        console.log(results[i].thumbnail);    
+    }
+})
+.catch(function (error) {
+    console.log(error);
 })
 
 //PORT listen
