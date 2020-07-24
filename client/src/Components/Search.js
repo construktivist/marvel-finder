aea78dfff46b83c035d275407dc23b7fe3dfcea4
Keegan Kelly <keegankelly1@gmail.com>
2020-07-24 11:21:24 -0500

    Added a reqest function to Search.js

--- client/src/Components/Search.js
+++ client/src/Components/Search.js
@@ -13,59 +13,57 @@ class Search extends React.Component {
         }
     }
 
-    //Called by handleSubmit to pass the api data back to App.js.
-    passResults() {
-        this.props.handleResults(this.state.results)
-    }
-
-    find = (searchType) => {
-
-        if (searchType === 'character') {
-            axios.get('/character', {
-                params: {
-                    characterName: this.state.search,
-                }
-                })
-                .then(function (response) {
-                    this.setState({
-                        results: response.data,
-                    });
-                    this.passResults();
-                }.bind(this))
-                .catch(function (error) {
-                })
-        }
-        else if (searchType === 'comics') {
-            axios.get('/character', {
-                params: {
-                    characterName: this.state.search,
-                }
-                })
-                .then(function (response) {
-                    this.setState({
-                        results: response.data,
-                    });
-                    this.passResults();
-                }.bind(this))
-                .catch(function (error) {
-                })
-        }
-        else {
-            console.log('ERROR: searchType does not match comics or characters');
-        }
-    }
-
-    //Handle text field changes
+    //Handle text field changes for search input
     handleChange = event => {
         this.setState({
             [event.target.id]: event.target.value,
         })
     }
 
-    //Make API call on submit
+    request = (type, params) => {
+        axios.get(type, params)
+        .then(response => {
+            this.setState({
+                results: response.data,
+            });
+            this.passResults();
+        })
+        .catch(error => {
+            console.log('ERROR: ' + error)
+        })
+    }
+
+    //Performs the GET request based on the searchType set by App.js
+    find = (searchType) => {
+
+        if (searchType === 'character') {
+            this.request('/character', {
+                    params: {
+                        characterName: this.state.search,
+                    }
+            });        
+        }
+        else if (searchType === 'comics') {
+            this.request('/comics', {
+                params: {
+                    titleStartsWith: this.state.search,
+                }
+            });
+        }
+        else {
+            console.log('ERROR: searchType: ' + searchType + ' does not match comics or characters');
+        }
+    }
+
+    //Make API call on submit using find()
     handleSubmit = event => {
         event.preventDefault();
         this.find(this.props.searchType);
+    }
+
+    //Called by handleSubmit to pass the api data back to App.js.
+    passResults() {
+        this.props.handleResults(this.state.results)
     }
 
     render () {
