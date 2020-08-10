6ad38eeac0c917dcdc156d74a5909070c2996f6d
Keegan Kelly <keegankelly1@gmail.com>
2020-08-10 13:01:35 -0500

    Added some style to Cover and working on url link.

--- client/src/Components/Cover.js
+++ client/src/Components/Cover.js
@@ -4,6 +4,7 @@ import '../Styles/Cover.css';
 // Displays an overview for Marvel Character profile. 
 class Cover extends React.Component {
     render () {
+        console.log(this.props.urls)
         return (
             <div className="cover expand col-xs-2">
                 <a href="#" target="_blank" rel="noopener noreferrer">
