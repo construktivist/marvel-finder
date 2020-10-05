c7dc4f6f352262af289ef4d93721bbe3f206879e
Keegan Kelly <keegankelly1@gmail.com>
2020-10-05 12:30:09 -0500

    Created Loading component.

---
+++ client/src/Components/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Loading extends React.Component {
+    render () {
+        return (
+            <div className="loading">
+                <h3>LOADING...</h3>
+            </div>
+        )
+    }
+}
+
+export default Loading;
