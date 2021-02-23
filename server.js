d77c9d1239b942e1cad803c85d08afd70cf836d9
Keegan Kelly <keegankelly1@gmail.com>
2021-02-23 11:43:59 -0600

    Deploying prod to heroku.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 //Express server that uses API helper to make calls with private API key.
 const express = require(`express`);
+const path = require('path');
 const marvel_api = require('./controllers/api')
 
 const app = express();
 
 //API requests
-// app.use('/', marvel_api)
+app.use('/', marvel_api)
 
 app.use(express.static(path.join(__dirname, 'build')));
 
